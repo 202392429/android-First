1805fc3054106a33ab112408cdc958d1295584a4
MMA <202392429@student.ttcollege.edu.sa>
2019-10-31 09:03:12 +0300

    add // First

--- app/src/main/java/applications/listadapter/MainActivity.java
+++ app/src/main/java/applications/listadapter/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+// Firstt
 public class MainActivity extends AppCompatActivity {
 
     EditText etFirstName,etLastName,etFavFood;
